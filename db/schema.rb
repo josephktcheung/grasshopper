ccbd73c93b1b88e8eba8ae58879f6fd6a4ce4581
josephktcheung <josephktcheung@gmail.com>
2014-04-09 12:59:35 +0800

    ADDED conversation model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140409020943) do
+ActiveRecord::Schema.define(version: 20140409040910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "apprenticeships", force: true do |t|
+    t.integer  "created_by"
+    t.integer  "created_for"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "conversations", force: true do |t|
+    t.integer  "created_by"
+    t.integer  "created_for"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "registrants", force: true do |t|
     t.string   "email"
