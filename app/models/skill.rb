0b44b2d9df939b6d6e6219801894468423f4bf3e
Annie Cheung <annieccheung@gmail.com>
2014-04-09 11:25:25 +0800

    CHANGED skills_user model name to proficiency.

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,7 +2,6 @@ class Skill < ActiveRecord::Base
   has_many  :skills_users
   has_many :users, through: :skills_users
 
-
   before_validation :downcase_name
 
   validates :skill_name, uniqueness: { case_sensitive: false }
