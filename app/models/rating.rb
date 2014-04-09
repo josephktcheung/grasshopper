404dc950439aa9a0cc7603bb705eca8bf7955b12
alex-zelikow <alex.zelikow@gmail.com>
2014-04-10 07:13:01 +0800

    UPDATED Apprenticeship/Rating with belongs_to class_name and DELETED prototype files

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,5 +1,7 @@
 class Rating < ActiveRecord::Base
-  belongs_to :rater
-  belongs_to :ratee
+
+  belongs_to :rater, class_name: "User"
+  belongs_to :ratee, class_name: "User"
   belongs_to :apprenticeship
+
 end
