61c26947ccf74356368b286a6ceb3605d077acb0
alex-zelikow <alex.zelikow@gmail.com>
2014-04-10 09:34:25 +0800

    ADDED rating model specs and validations required to pass them

--- spec/models/apprenticeship_spec.rb
+++ spec/models/apprenticeship_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe Apprenticeship do
 
+  before :each do
+    @user1 = User.create(first_name: 'Grass', last_name: 'Hopper', email: 'gh@ga.co', password: '123', password_confirmation: '123', role: 'master')
+    @user2 = User.create(first_name: 'App', last_name: 'rentice', email: 'ap@ga.co', password: '123', password_confirmation: '123', role: 'apprentice')
+    @apprenticeship = Apprenticeship.create(master: @user1, apprentice: @user2, end_date: (-1.month.from_now))
+    @rating = Rating.create(rater: @user1.id, ratee: @user2.id, apprenticeship: @apprenticeship)
+  end
+
   describe "master" do
     it "should not have an existing active status with the grasshopper"
     it "should not be valid without a user who has a master role"
