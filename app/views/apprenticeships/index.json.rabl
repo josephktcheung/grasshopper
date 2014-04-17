25c85426bf0ec905ac2c22ad89e33732bd94b75e
josephktcheung <josephktcheung@gmail.com>
2014-04-18 01:52:41 +0800

    ADDED apprenticeship/new json rabl and test cases

---
+++ app/views/apprenticeships/index.json.rabl
@@ -0,0 +1,46 @@
+object false
+
+node :links do
+  {
+    :"apprenticeships.master" => {
+      href: root_url + "api/users/{apprenticeships.master}",
+      type: "users"
+    },
+    :"apprenticeships.apprentice" => {
+      href: root_url + "api/users/{apprenticeships.apprentice}",
+      type: "users"
+    },
+    :"apprenticeships.ratings" => {
+      href: root_url + "api/ratings/{apprenticeships.ratings}",
+      type: "ratings"
+    }
+  }
+end
+
+child @apprenticeships do
+  extends "apprenticeships/show"
+end
+
+node :linked do
+  users = @apprenticeships.map { |apprenticeship| [apprenticeship.master.id, apprenticeship.apprentice.id] }
+  users.flatten!.uniq!
+  {
+    users: users.map do |user|
+      {
+        href:     user_url(user),
+        id:       user,
+        username: User.find(user).username
+      }
+    end,
+
+    ratings: Rating.all.map do |rating|
+      {
+        href:   rating_url(rating),
+        id:     rating.id,
+        rater:  rating.rater.id,
+        ratee:  rating.ratee.id,
+        rating: rating.rating
+      }
+    end
+  }
+end
---
+++ spec/controllers/apprenticeships_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe ApprenticeshipsController, :type => :api do
+
+  render_views
+
+  before :each do
+    @user1 = User.create(first_name: 'Grass', last_name: 'Hopper', email: 'gh@ga.co', password: '123', password_confirmation: '123', role: 'master')
+    @user2 = User.create(first_name: 'App', last_name: 'Rentice', email: 'ap@ga.co', password: '123', password_confirmation: '123', role: 'apprentice')
+    @apprenticeship = Apprenticeship.create(master: @user1, apprentice: @user2, end_date: 1.day.from_now)
+    @rating1 = Rating.create(rater: @user1, ratee: @user2, apprenticeship: @apprenticeship, rating: 5)
+    @rating2 = Rating.create(rater: @user2, ratee: @user1, apprenticeship: @apprenticeship, rating: 3)
+  end
+
+  describe 'GET index' do
+
+    it "returns list of apprenticeships" do
+      get :index, :format => :json
+      expect(response.status).to eq 200
+      expect(JSON.load(response.body)["apprenticeships"][0]["end_date"]).to eq @apprenticeship.end_date.iso8601(3)
+      expect(JSON.load(response.body)["apprenticeships"][0]["links"]["master"]).to eq @user1.id
+      expect(JSON.load(response.body)["apprenticeships"][0]["links"]["apprentice"]).to eq @user2.id
+      expect(JSON.load(response.body)["apprenticeships"][0]["links"]["ratings"]).to eq [@rating1.id, @rating2.id]
+    end
+
+  end
+
+  describe "GET show" do
+
+    it "return specific apprenticeship" do
+      get :show, id: Apprenticeship.first.id, :format => :json
+      expect(response.status).to eq 200
+    end
+
+  end
+end
