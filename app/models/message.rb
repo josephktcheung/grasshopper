61ac3353b603cd89fcdae85dd271c88581fd8cd6
josephktcheung <josephktcheung@gmail.com>
2014-04-26 21:35:01 +0800

    FIXED bug in message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,8 @@ class Message < ActiveRecord::Base
   belongs_to :recipient, class_name: "User", foreign_key: "recipient_id"
   belongs_to :sender, class_name: "User", foreign_key: "sender_id"
 
-  validates :recipient_id, presence: true
-  validates :sender_id, presence: true
+  validates :recipient, presence: true
+  validates :sender, presence: true
   validates :conversation, presence: true
   validates :content, presence: true
   validate :message_to_self?
