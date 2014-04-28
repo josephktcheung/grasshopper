66264ecf688a2dd70ad8061e49f0d6bee5d7de5a
Annie Cheung <annieccheung@gmail.com>
2014-04-28 16:09:07 +0800

    FIXED apprenticeship test cases for master and apprentice.

--- spec/models/apprenticeship_spec.rb
+++ spec/models/apprenticeship_spec.rb
@@ -6,7 +6,6 @@ describe Apprenticeship do
     @user1 = User.create(first_name: 'Grass', last_name: 'Hopper', email: 'gh@ga.co', password: '123', password_confirmation: '123', role: 'master')
     @user2 = User.create(first_name: 'App', last_name: 'Rentice', email: 'ap@ga.co', password: '123', password_confirmation: '123', role: 'apprentice')
     @apprenticeship = Apprenticeship.create(master: @user1, apprentice: @user2, end_date: 1.second.from_now, status: 'active')
-    @apprenticeship_invalid = Apprenticeship.create(master: @user2, apprentice: @user1, end_date: 1.second.from_now, status: 'active')
     @rating = Rating.create(rater: @user1, ratee: @user2, apprenticeship: @apprenticeship, rating: 5)
   end
 
@@ -25,18 +24,15 @@ describe Apprenticeship do
   end
 
   describe "master" do
-    it "should not be valid without a user who has a master role" do
+    it "should not be valid without a user who has a master role in master_id" do
       @user1.role = 'apprentice'
       expect(@apprenticeship).to_not be_valid
     end
 
-    it "should not be valid with a master who doesn't have a master role" do
-      expect(@apprenticeship_invalid).to_not be_valid
-    end
   end
 
   describe "apprentice" do
-    it "should not be valid without a user who has a apprentice role" do
+    it "should not be valid without a user who has a apprentice role in apprentice_id" do
       @user2.role = 'master'
       expect(@apprenticeship).to_not be_valid
     end
